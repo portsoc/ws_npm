8a2af71b7334f6074e62a2aa4ccc700c2543507e
Jacek Kopecký <jacek@jacek.cz>
2016-11-15 22:10:04 +0000

    add example of http server with express

---
+++ examples/http-express.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+ 
+app.get('/hello', function (req, res) {
+  res.send('Hello ' + (req.query.name || 'anonymous') + '!');
+})
+    
+app.listen(8080)
