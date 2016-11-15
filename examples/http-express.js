6a1aae780b82347d0bbd351d8aaf528befda325e
Jacek Kopecký <jacek@jacek.cz>
2016-11-15 22:29:19 +0000

    :art: add semicolons

--- examples/http-express.js
+++ examples/http-express.js
@@ -1,8 +1,8 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
  
 app.get('/hello', function (req, res) {
   res.send('Hello ' + (req.query.name || 'anonymous') + '!');
-})
+});
     
-app.listen(8080)
+app.listen(8080);
