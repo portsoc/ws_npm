c95e50ef39ca364d66e08806c7c24d643e96d038
Jacek Kopecký <KopeckyJ@uni.ds.port.ac.uk>
2016-11-16 14:44:52 +0000

    move express tests to be first

--- test.js
+++ test.js
@@ -6,58 +6,13 @@ var dir = "./worksheet/";
 var pathUtil = "utility.js";
 var pathWeb = "webserver.js";
 
-/**
- * Create a file `utility.js` within
- * the worksheet folder.
- */
-test(
-  "Create a file `" + pathUtil + "`",
-  function () {
-    try {
-      fs.accessSync(dir+pathUtil, fs.F_OK);
-      ok(true, pathUtil + " created");
-    } catch (e) {
-      ok(false, pathUtil + " is missing - please create it");
-    }
-});
-
-
-
-
-/**
- * todo comment
- */ 
-test(
-  "Use the underscore package.",
-  function () {
-    var _ = require('underscore');
-    ok(_, "The underscore package should be installed.");
-    var package = require('./package.json');
-    ok(package.dependencies.underscore, "The underscore package should be a dependency of ws_npm.");
-});
-
-
-
-
-/**
- * todo comment
- */
-test(
-  "Create a range function.",
-  function () {
-    var util = require(dir+pathUtil);
-    equal(util.range([4,3]), 1, "[4,3] has a range of 1");
-    equal(util.range([3]), 0, "[3] has a range of 0");
-    equal(util.range([3, -1, 5]), 6, "[3, -1, 5] has a range of 6");
-    // todo check that underscore.max and underscore.min are in use
-});
 
 
 
 /**
  * Create a file `webserver.js` within the worksheet folder.
  *
- * Reuse the code from the `http-express.js` example to implement
+ * Reuse the code from the `http-add.js` example to implement
  * a web server that can listen on port 8080.
  *
  * The server should respond to requests on `/subtract`, accepting
@@ -85,27 +40,25 @@ test(
 });
 
 
-// todo adapt the tests to /subtract
-// todo also check that express is being used (can we check that?)
 test(
-  "Add two numbers for the path /add",
+  "Subtract two numbers for the path /subtract",
   function () {
     require(dir+pathWeb);
     var options = {
       host: 'localhost',
       port: '8080',
       method: 'GET',
-      path: '/add?a=2&b=3.4',
+      path: '/subtract?a=2&b=3.4',
     };
 
     stop();
 
     var req = http.request(options, function(response) {
-      equal(response.statusCode, 200, 'successful /add should return status code 200');
+      equal(response.statusCode, 200, 'successful /subtract should return status code 200');
       var str = '';
       response.on('data', function(chunk) { str += chunk; });
       response.on('end', function() {
-        equal(str.trim(), '5.4', 'calling /add?a=2&b=3.4 returns 5.4');
+        equal(str.trim(), '-1.4', 'calling /subtract?a=2&b=3.4 returns -1.4');
         start();
       });
     });
@@ -144,3 +97,55 @@ test(
     req.end();
   }
 );
+
+
+
+
+
+
+
+/**
+ * Create a file `utility.js` within
+ * the worksheet folder.
+ */
+test(
+  "Create a file `" + pathUtil + "`",
+  function () {
+    try {
+      fs.accessSync(dir+pathUtil, fs.F_OK);
+      ok(true, pathUtil + " created");
+    } catch (e) {
+      ok(false, pathUtil + " is missing - please create it");
+    }
+});
+
+
+
+
+/**
+ * todo comment
+ */
+test(
+  "Use the underscore package.",
+  function () {
+    var _ = require('underscore');
+    ok(_, "The underscore package should be installed.");
+    var package = require('./package.json');
+    ok(package.dependencies.underscore, "The underscore package should be a dependency of ws_npm.");
+});
+
+
+
+
+/**
+ * todo comment
+ */
+test(
+  "Create a range function.",
+  function () {
+    var util = require(dir+pathUtil);
+    equal(util.range([4,3]), 1, "[4,3] has a range of 1");
+    equal(util.range([3]), 0, "[3] has a range of 0");
+    equal(util.range([3, -1, 5]), 6, "[3, -1, 5] has a range of 6");
+    // todo check that underscore.max and underscore.min are in use
+});
