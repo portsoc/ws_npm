93a813e7c085556f71bbafad8997ac6240a5e921
Rich Boakes <rjb@port.ac.uk>
2017-11-20 15:51:01 +0000

    Small formatting change and extra comments.

--- test.js
+++ test.js
@@ -29,23 +29,35 @@ QUnit.module("Web Server");
  * Running the tests starts your web server, but if you want to try it in
  * your browser, you need to start the webserver explicitly, with the command
  * `node worksheet/webserver`
+ *
+ * Use the `npm install underscore --save` command in the `worksheet` directory.
  */
 QUnit.test(
   "Create a file `" + pathWeb + "` in `" + dir + "`",
   function () {
     try {
-      fs.accessSync(dir+pathWeb, fs.F_OK);
+      // Try to access the file.  If it is there, we can continue to
+      // the next line.  If it is NOT there, an error will be thrown.
+      fs.accessSync(dir + pathWeb, fs.F_OK);
       ok(true, pathWeb + " created");
     } catch (e) {
       ok(false, pathWeb + " is missing - please create it");
     }
-});
+  });
 
 
 QUnit.test(
   "Subtract two numbers for the path /subtract",
   function () {
-    require(dir+pathWeb);
+    // this is the file you created in the Web Server task
+    // when we 'require' it, it loads and runs.
+    require(dir + pathWeb);
+
+    // begin stalling (we use stop() and start() in QUnit
+    // // to hand tests that feature callbacks.
+    stop();
+
+    // build an options object that can be used to make an HTTP request
     const options = {
       host: 'localhost',
       port: '8080',
@@ -53,16 +65,22 @@ QUnit.test(
       path: '/subtract?a=2&b=3.4',
     };
 
-    stop();
-
-    const req = http.request(options, function(response) {
-      equal(response.statusCode, 200, 'successful /subtract should return status code 200');
+    //make the http request to the server.
+    const req = http.request(options, function (response) {
+      equal(response.statusCode, 200, 'A successful call to /subtract should return a status code of 200');
       let str = '';
-      response.on('data', function(chunk) { str += chunk; });
-      response.on('end', function() {
-        equal(str.trim(), '-1.4', 'calling /subtract?a=2&b=3.4 returns -1.4');
+
+      response.on('data', function (chunk) {
+        // combine the parts of the response (if it happens to be sent in parts)
+        str += chunk;
+      });
+
+      response.on('end', function () {
+        // when the last part arrives we can quit stalling.
+        equal(str.trim(), '-1.4', 'Test that calling /subtract?a=2&b=3.4 returns -1.4');
         start();
       });
+
     });
     req.on('error', function (e) {
       ok(false);
@@ -76,7 +94,7 @@ QUnit.test(
 QUnit.test(
   "Return a 404 for all non-existent paths",
   function () {
-    require(dir+pathWeb);
+    require(dir + pathWeb);
     const options = {
       host: 'localhost',
       port: '8080',
@@ -87,8 +105,8 @@ QUnit.test(
     expect(1);
     stop();
 
-    const req = http.request(options, function(response) {
-      equal(response.statusCode, 404, 'server should return 404 for /nothere');
+    const req = http.request(options, function (response) {
+      equal(response.statusCode, 404, 'The server should return 404 for /nothere');
       start();
     });
     req.on('error', function (e) {
@@ -102,46 +120,44 @@ QUnit.test(
 
 
 
+
 QUnit.module("Packages");
-
-
 /**
  * Using `npm init`, create a `package.json` in the worksheet folder.
  * The name of the package should be worksheet and `main` should be `utility.js`.
  *
  * Create the file `utility.js` within the worksheet folder.
  * In that file export a function called `range` that takes one parameter,
- * an array of numbers. To implement the function, find the `underscore`
- * package and use its `max` and `min` functions to return the range of
- * the provided array, i.e. its maximal value minus its minimal value;
- * make sure to add the underscore package as a dependency in the top-level package.json
+ * an array of numbers.
  *
- * Use the `npm install express --save` command in the `worksheet` directory
- * to install express, so your webserver above works.
- * Do a similar thing to install the underscore package.
+ * To implement the function, find the `underscore` package in npm and use
+ * its `max` and `min` functions to return the range of the provided array,
+ * i.e. its maximal value minus its minimal value;
+ *
+ * Use the `npm install underscore --save` command in the `worksheet` directory.
  */
 QUnit.test(
   "Create a file `" + pathUtil + "`",
   function () {
     try {
-      fs.accessSync(dir+pathUtil, fs.F_OK);
+      fs.accessSync(dir + pathUtil, fs.F_OK);
       ok(true, pathUtil + " created");
     } catch (e) {
       ok(false, pathUtil + " is missing - please create it");
     }
-});
+  });
 
 
 QUnit.test(
   "Use `npm init` in `" + dir + "` to create a file `" + pathPkg + "`",
   function () {
     try {
-      fs.accessSync(dir+pathPkg, fs.F_OK);
+      fs.accessSync(dir + pathPkg, fs.F_OK);
       ok(true, pathPkg + " created");
     } catch (e) {
       ok(false, pathPkg + " is missing - please create it");
     }
-});
+  });
 
 
 QUnit.test(
@@ -150,7 +166,7 @@ QUnit.test(
     const pkg = require('./worksheet/package.json');
     equal(pkg.name, 'worksheet', "`worksheet/package.json` should have the name 'worksheet'.");
     equal(pkg.main, 'utility.js', "`worksheet/package.json` should have 'main' set to 'utility.js'.");
-});
+  });
 
 
 QUnit.test(
@@ -173,13 +189,13 @@ QUnit.test(
       _ = require('./worksheet/node_modules/underscore');
     } catch (e) {}
     ok(_, 'underscore should be installed inside worksheet');
-});
+  });
 
 
 QUnit.test(
   "Create a `range` function that accepts an array and returns the size of the range between the largest and smallest numbers.",
   function () {
-    const util = require(dir+pathUtil);
+    const util = require(dir + pathUtil);
     let _;
     try {
       _ = require('./worksheet/node_modules/underscore');
@@ -192,10 +208,16 @@ QUnit.test(
     const oldMax = _.max;
     let minCount = 0;
     let maxCount = 0;
-    _.min = function (arr) { minCount++; return oldMin(arr); }
-    _.max = function (arr) { maxCount++; return oldMax(arr); }
+    _.min = function (arr) {
+      minCount++;
+      return oldMin(arr);
+    }
+    _.max = function (arr) {
+      maxCount++;
+      return oldMax(arr);
+    }
 
-    equal(util.range([4,3]), 1, "[4,3] has a range of 1");
+    equal(util.range([4, 3]), 1, "[4,3] has a range of 1");
     equal(minCount, 1, "use underscore.min in your range function");
     equal(maxCount, 1, "use underscore.max in your range function");
     equal(util.range([3]), 0, "[3] has a range of 0");
@@ -204,4 +226,4 @@ QUnit.test(
     equal(util.range([3, -1, 5]), 6, "[3, -1, 5] has a range of 6");
     equal(minCount, 3, "use underscore.min in your range function");
     equal(maxCount, 3, "use underscore.max in your range function");
-});
+  });
