bcc93fa4fd1afa3fc8eeede058840b10ba8880a3
Jacek Kopecky <jacek@jacek.cz>
2020-01-31 13:28:40 +0000

    update examples

--- examples/http-express-route.js
+++ examples/http-express-route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+app.use('/', express.static('public'));
 
 app.get('/hello', function (req, res) {
-  res.send('Hello ' + (req.query.name || 'anonymous') + '!');
+ res.send('Hello ' + (req.query.name || 'anonymous') + '!');
 });
 
 app.listen(8080);
